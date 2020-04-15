ae39176d913630ab907346da393e3337351d814f
Batuzzai <gonsalazarm@gmail.com>
2020-04-15 17:00:30 -0400

    Update since.php

--- since.php
+++ since.php
@@ -3,7 +3,7 @@ $fecha1= new DateTime("2019-11-13");
 $fecha2= new DateTime(date("Y")."-".date("m")."-".date("d"));
 $diff = $fecha1->diff($fecha2);
 
-e$months = $diff->format("%m");
+$months = $diff->format("%m");
 $years = $diff->format("%y")*12;
 
 echo ($months+$years) . ' meses';
