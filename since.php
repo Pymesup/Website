5fee23ed9eacacee9778942dee4bac80d5b8574b
Batuzzai <gonsalazarm@gmail.com>
2020-04-15 13:46:30 -0400

    corrección funcionamiento

--- since.php
+++ since.php
@@ -1,6 +1,6 @@
 <?php
 $fecha1= new DateTime("2019-11-13");
-$fecha2= date('Y-m-d');
+$fecha2= new DateTime(date("Y")."-".date("m")."-".date("d"));
 $diff = $fecha1->diff($fecha2);
 
 echo $diff->days . ' dias';
